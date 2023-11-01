12cf6f064ded4e02bcb8ded24489e006dc831956
Emily Wertheimer <71396046+Emily-Wertheimer@users.noreply.github.com>
2023-11-01 15:54:19 -0400

    Update run_sPredix_Freeze3BrainCell.sh

--- run_sPredix_Freeze3BrainCell.sh
+++ run_sPredix_Freeze3BrainCell.sh
@@ -17,13 +17,13 @@ trait=AN
 trait_filename=freeze3.AN.GWAS.sumstats.clean
 path_GWAS=/home/ekw28/AN-CellTypeS-Predixcan/freeze3_cellTypes_repo #/freeze3.AN.GWAS.sumstats.clean
 
+
 # Set locations of predictor models (provide an actual value for cellType_predix)
 tissueType_predix=("BasoAmyg_" "dACC_" "DLPFC_" "MedialAmyg_")
 cellType_predix=Excitatory_Neurons 
 
-PREDICT_DB_path="/home/ekw28/AN-CellTypeS-Predixcan/freeze3_cellTypes_repo" #/${tissue}${cellType_predix}.db"
-COVARIANCE_PATH="/home/ekw28/AN-CellTypeS-Predixcan/freeze3_cellTypes_repo" #/${tissue}${cellType_predix}.txt.gz"
-
+PREDICT_DB_path=/home/ekw28/AN-CellTypeS-Predixcan/freeze3_cellTypes_repo #/${tissue}${cellType_predix}.db
+COVARIANCE_PATH=/home/ekw28/AN-CellTypeS-Predixcan/freeze3_cellTypes_repo #/${tissue}${cellType_predix}.txt.gz
 
 # Specify GWAS column names (modify these based on your GWAS data)
 snp_column="rs.id.dbSNP151.GRCh38p7"
@@ -33,9 +33,11 @@ pvalue_column="p"
 or_column="OR"
 
 # Run SPrediXcan for e/ tissue type
+LOOPINGFILE=/home/ekw28/AN-CellTypeS-Predixcan/freeze3_cellTypes_repo/Excitatory_Neurons
+cd $scratch
 for tissue in ${tissueType_predix[@]}; do
     python ${SPREDIX_path} \
-    --model_db_path ${PREDICT_DB_path}/${tissue}"/"${cellType_predix}.db \
+    --model_db_path ${PREDICT_DB_path}/${tissue}/${cellType_predix}.db \
     --covariance ${COVARIANCE_PATH} ${tissue} ${cellType_predix}.txt.gz \
     --gwas_folder ${path_GWAS} \
     --gwas_file_pattern ${trait_filename} \
@@ -45,4 +47,4 @@ for tissue in ${tissueType_predix[@]}; do
     --pvalue_column ${pvalue_column} \
     --or_column ${or_column} \
     --output_file ${outpath}/${trait}_${tissueType_predix}${cellType_predix}.carina_cell_type
-done 
+done <${LOOPINGFILE}
