c30c2f9cbe4c583eacf474eb2af7e7526586792b
Emily Wertheimer <71396046+Emily-Wertheimer@users.noreply.github.com>
2023-11-02 14:55:32 -0400

    Update run_sPredix_Freeze3BrainCell.sh

--- run_sPredix_Freeze3BrainCell.sh
+++ run_sPredix_Freeze3BrainCell.sh
@@ -23,28 +23,25 @@ conda activate py3_env_spredix
 # Set software file locations
 outpath=/home/ekw28/AN-CellTypeS-Predixcan/freeze3_cellTypes_repo
 scratch=/home/ekw28/AN-CellTypeS-Predixcan/freeze3_cellTypes_repo/scratch
-cd /vast/palmer/home.mccleary/ekw28/AN-CellTypeS-Predixcan/freeze3_cellTypes_repo
+cd $scratch
 
 # Set GWAS trait information
 trait=AN
 trait_filename=freeze3.AN.GWAS.sumstats.clean
-path_GWAS=/home/ekw28/AN-CellTypeS-Predixcan/freeze3_cellTypes_repo #/freeze3.AN.GWAS.sumstats.clean
+path_GWAS=/home/ekw28/AN-CellTypeS-Predixcan/freeze3_cellTypes_repo 
 
 ######################################################################################
 ### submit 1 tissue per job
 tissue_predix=BasoAmyg_Excitatory_Neurons # options are: BasoAmyg_, dACC_, DLPFC_, MedialAmyg_
-PREDICT_DB_path=/home/ekw28/AN-CellTypeS-Predixcan/freeze3_cellTypes_repo/Excitatory_Neurons/BasoAmyg_Excitatory_Neurons_weights.db
-COVARIANCE_PATH=/home/ekw28/AN-CellTypeS-Predixcan/freeze3_cellTypes_repo/BasoAmyg_Excitatory_Neurons_LD_covariance_matrix.txt.gz
+PREDICT_DB_path=/home/ekw28/AN-CellTypeS-Predixcan/freeze3_cellTypes_repo/Excitatory_Neurons/BasoAmyg_Excitatory_Neurons_weights.db 
+COVARIANCE_PATH=/home/ekw28/AN-CellTypeS-Predixcan/freeze3_cellTypes_repo/Excitatory_Neurons/BasoAmyg_Excitatory_Neurons_LD_covariance_matrix.txt
 
-## Specify GWAS column names (modify these based on your GWAS data)
-snp_column="rs_id_dbSNP151_GRCh38p7"
-effect_allele_column="effect_allele"
-non_effect_allele_column="noneffect_allele"
-pvalue_column="p"
-or_column="OR"
+## double check gwas colnames
+#Change the snp_column, effect_allele_column, non_effect_allele_column, pvalue_column, or_column to match the colnames in the GWAS
+
 
 ## Run spredixcan
-python ${SPREDIX_path} --model_db_path ${PREDICT_DB_path} --covariance ${COVARIANCE_PATH} --gwas_folder ${path_GWAS} --gwas_file_pattern ${trait_filename} --snp_column rs_id_dbSNP151_GRCh38p7 --effect_allele_column effect_allele --non_effect_allele_column noneffect_allele --pvalue_column p --or_column OR --output_file ${outpath}/${trait}_${tissue_predix}.carina.cell.type​
+python ${SPREDIX_path} --model_db_path ${PREDICT_DB_path} --covariance ${COVARIANCE_PATH} --gwas_folder ${path_GWAS} --gwas_file_pattern ${trait_filename} --snp_column rs_id_dbSNP151_GRCh38p7 --effect_allele_column effect_allele --non_effect_allele_column noneffect_allele --pvalue_column p --or_column OR --output_file ${outpath}/${trait}_${tissue_predix}.carina.cell.type
 
 ######################################################################################
 
