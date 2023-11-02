56da9d77e185c12e6b2e0553fa61c06525d26adc
Emily Wertheimer <71396046+Emily-Wertheimer@users.noreply.github.com>
2023-11-02 15:25:27 -0400

    Update clean_gwas_freeze3_gtex.R

--- clean_gwas_freeze3_gtex.R
+++ clean_gwas_freeze3_gtex.R
@@ -6,8 +6,7 @@ if(!require(R.utils)) {install.packages("R.utils")}; library(R.utils)
 ## load data
 stats_raw <- as.data.frame(fread('/home/ekw28/AN-CellTypeS-Predixcan/sPrediXcan_AN/data/freeze3sumStats/daner_AN.meta.gz', quote="", stringsAsFactors=F)) 
 
-hg_map <- data.frame(fread('/gpfs/gibbs/pi/huckins/resources/GTEx_map_37.txt.gz'
-,header=T,  sep="\t", quote = "",stringsAsFactors=F))
+hg_map <- data.frame(fread('/gpfs/gibbs/pi/huckins/resources/GTEx_map_37.txt.gz', header=T,  sep="\t", quote = "",stringsAsFactors=F))
 
 ## change chr & pos to factors
 stats_raw$CHR <- as.factor(stats_raw$CHR) 
@@ -22,7 +21,7 @@ colnames(stats_raw)[5]<-'alt'
 ## join sumstats w/ human genome map by chromosome & position 
 sumstats_merge_GTEx <- inner_join(stats_raw,hg_map,by=c('CHR','BP'='pos_37'))
 
-colnames(sumstats_merge_GTEx_rsid)
+colnames(sumstats_merge_GTEx)
 # [1] "CHR"                     "SNP"                    
  #[3] "BP"                      "ref.x"                  
 # [5] "alt.x"                   "FRQ_A_24145"            
@@ -37,30 +36,30 @@ colnames(sumstats_merge_GTEx_rsid)
 #[23] "rs_id_dbSNP151_GRCh38p7"
 
 
-colnames(sumstats_merge_GTEx_rsid)[4] <- 'noneffect_allele'
-colnames(sumstats_merge_GTEx_rsid)[5] <- 'effect_allele'
-colnames(sumstats_merge_GTEx_rsid)[11] <- 'p'
-colnames(sumstats_merge_GTEx_rsid)[22] <- 'variant.id.hg38'
-colnames(sumstats_merge_GTEx_rsid)[23] <- 'rs.id.dbSNP151.GRCh38p7'
-colnames(sumstats_merge_GTEx_rsid)[10] <- 'effect_error'
+colnames(sumstats_merge_GTEx)[4] <- 'noneffect_allele'
+colnames(sumstats_merge_GTEx)[5] <- 'effect_allele'
+colnames(sumstats_merge_GTEx)[11] <- 'p'
+colnames(sumstats_merge_GTEx)[22] <- 'variant.id.hg38'
+colnames(sumstats_merge_GTEx)[23] <- 'rs_id_dbSNP151_GRCh38p7'
+colnames(sumstats_merge_GTEx)[10] <- 'effect_error'
 
-dim(sumstats_merge_GTEx_rsid) #dim: 6804937      23
+dim(sumstats_merge_GTEx) #dim: 6804937      23
 
 
 ######################################## 
 
-sumstats_save_filt <- sumstats_merge_GTEx_rsid # %>% dplyr::rename(effect_allele='alt', noneffect_allele='ref')
+sumstats_save_filt <- sumstats_merge_GTEx # %>% dplyr::rename(effect_allele='alt', noneffect_allele='ref')
 
 #Save new columns
-sumstats_save_final <- sumstats_save_filt %>% select(all_of(c('rs.id.dbSNP151.GRCh38p7','effect_allele','noneffect_allele','p','OR')))
+sumstats_save_final <- sumstats_save_filt %>% select(all_of(c('rs_id_dbSNP151_GRCh38p7','effect_allele','noneffect_allele','p','OR')))
 
 #Combine into file name to later pull out in linux when we run s-predixcan
 #filename_new_here <- paste0(paste(subfile_name,trait_name,paste(effect_size,effect_error,sep='_'),sep='-'),'.txt.gz')
 #print(filename_new_here)
 #couldnt get previous 2 lines to work so did this instead: 
-freeze3.gwas.sumstats.clean <- sumstats_save_final
+freeze3.gwas.sumstats.clean2 <- sumstats_save_final
 ​head(anGWAS2019.sumstats.clean)
 
 #Save
 #write.table(freeze3.gwas.sumstats.clean, file = "/vast/palmer/home.mccleary/ekw28/freeze3.gwas.sumstats.clean")
-write.table(freeze3.gwas.sumstats.clean, file=/home/ekw28/AN-CellTypeS-Predixcan/freeze3.gwas.sumstats.clean2, sep = "\t", col.names=T, row.names = F,quote=F)
+write.table(freeze3.gwas.sumstats.clean2, file="/home/ekw28/AN-CellTypeS-Predixcan/freeze3.gwas.sumstats.clean2", sep = "\t", col.names=T, row.names = F,quote=F)
